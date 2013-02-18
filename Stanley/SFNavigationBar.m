ec5b9544cf60d667a5e444ba84043447deb07b26
Eric Horacek <horacek.eric@gmail.com>
2013-02-17 20:11:50 -0700

    Adds a direction label to the navigation bar indicating the direction navigation pane will slide

--- Stanley/SFNavigationBar.m
+++ Stanley/SFNavigationBar.m
@@ -13,12 +13,26 @@
 
 @interface SFNavigationBar ()
 
+@property (nonatomic, strong) UILabel *navigationPaneDirectionLabel;
+
 + (CGFloat)barHeight;
 + (UIFont *)titleTextFont;
+
+- (void)updateNavigationPaneLabelForOrientation:(UIDeviceOrientation)orientation;
+- (void)didChangeOrientation:(NSNotification *)notification;
 
 @end
 
 @implementation SFNavigationBar
+
+#pragma mark - NSObject
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark - UIView
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -32,6 +46,22 @@
             UITextAttributeTextShadowColor : [UIColor blackColor],
             UITextAttributeTextShadowOffset : [NSValue valueWithCGSize:CGSizeMake(0.0, -1.0)]
         };
+        
+        self.navigationPaneDirectionLabel = [UILabel new];
+        self.navigationPaneDirectionLabel.text = @"\U000025BE";
+        self.navigationPaneDirectionLabel.backgroundColor = [UIColor clearColor];
+        self.navigationPaneDirectionLabel.font = [[SFStyleManager sharedManager] symbolSetFontOfSize:((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) ? 11.0 : 9.0)];
+        self.navigationPaneDirectionLabel.textColor = [UIColor whiteColor];
+        self.navigationPaneDirectionLabel.shadowColor = [UIColor blackColor];
+        self.navigationPaneDirectionLabel.shadowOffset = CGSizeMake(0.0, -1.0);
+        self.navigationPaneDirectionLabel.userInteractionEnabled = NO;
+        [self addSubview:self.navigationPaneDirectionLabel];
+        
+        [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(didChangeOrientation:)
+                                                     name:UIDeviceOrientationDidChangeNotification
+                                                   object:[UIDevice currentDevice]];
     }
     return self;
 }
@@ -39,23 +69,35 @@
 - (void)layoutSubviews
 {
     [super layoutSubviews];
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-        for (UIView *subview in self.subviews) {
-            // Hacky...
-            if ([subview isKindOfClass:NSClassFromString(@"UINavigationItemView")]) {
+    
+    [self updateNavigationPaneLabelForOrientation:(UIDeviceOrientation)[[UIApplication sharedApplication] statusBarOrientation]];
+    
+    for (UIView *subview in self.subviews) {
+        // Hacky...
+        if ([subview isKindOfClass:NSClassFromString(@"UINavigationItemView")]) {
+            if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
                 CGRect navigationItemFrame = subview.frame;
                 navigationItemFrame.origin.y = 13.0;
                 navigationItemFrame.size.height = self.class.titleTextFont.lineHeight;
                 subview.frame = navigationItemFrame;
+            }
 #if defined(LAYOUT_DEBUG)
-                subview.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:0.5];
+            subview.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:0.5];
 #endif
-            }
-            else if ([subview isKindOfClass:UIButton.class]) {
-                CGRect buttonFrame = subview.frame;
+        }
+        // If it's the right navigation button
+        else if ([subview isKindOfClass:UIButton.class] && (CGRectGetMinX(subview.frame) < CGRectGetMidX(self.frame))) {
+            CGRect buttonFrame = subview.frame;
+            if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
                 buttonFrame.origin.y = 6.0;
-                subview.frame = buttonFrame;
             }
+            subview.frame = buttonFrame;
+            
+            [self.navigationPaneDirectionLabel sizeToFit];
+            CGRect navigationPaneDirectionLabelFrame = self.navigationPaneDirectionLabel.frame;
+            navigationPaneDirectionLabelFrame.origin.x = (CGRectGetMaxX(buttonFrame) - 3.0);
+            navigationPaneDirectionLabelFrame.origin.y = ((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) ? 22.0 : 17.0);
+            self.navigationPaneDirectionLabel.frame = navigationPaneDirectionLabelFrame;
         }
     }
 }
@@ -64,6 +106,25 @@
 {
     CGSize newSize = CGSizeMake([super sizeThatFits:size].width, self.class.barHeight);
     return newSize;
+}
+
+#pragma mark - SFNavigationBar
+
+- (void)updateNavigationPaneLabelForOrientation:(UIDeviceOrientation)orientation
+{
+    if (UIDeviceOrientationIsPortrait(orientation)) {
+        self.navigationPaneDirectionLabel.transform = CGAffineTransformMakeRotation(0.0);
+        self.navigationPaneDirectionLabel.shadowOffset = CGSizeMake(0.0, -1.0);
+    } else {
+        self.navigationPaneDirectionLabel.transform = CGAffineTransformMakeRotation(-M_PI_2);
+        self.navigationPaneDirectionLabel.shadowOffset = CGSizeMake(1.0, 0.0);
+    }
+}
+
+- (void)didChangeOrientation:(NSNotification *)notification
+{
+    UIDevice *device = [notification object];
+    [self updateNavigationPaneLabelForOrientation:device.orientation];
 }
 
 + (CGFloat)barHeight
