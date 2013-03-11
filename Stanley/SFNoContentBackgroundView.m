98916ce44009fa208eb1cd9d7bae8e304dc8fdf4
Eric Horacek <horacek.eric@gmail.com>
2013-03-10 23:46:25 -0600

    No content background sizing updates

--- Stanley/SFNoContentBackgroundView.m
+++ Stanley/SFNoContentBackgroundView.m
@@ -32,6 +32,7 @@
         self.title.innerShadowOffset = CGSizeMake(0.0, 1.0);
         self.title.shadowColor = [UIColor blackColor];
         self.title.shadowOffset = CGSizeMake(0.0, 3.0);
+        self.title.layer.masksToBounds = NO;
         [self addSubview:self.title];
         
         self.icon = [FXLabel new];
@@ -45,12 +46,13 @@
         self.icon.innerShadowOffset = CGSizeMake(0.0, 1.0);
         self.icon.shadowColor = [UIColor blackColor];
         self.icon.shadowOffset = CGSizeMake(0.0, 3.0);
+        self.icon.layer.masksToBounds = NO;
         [self addSubview:self.icon];
         
         self.subtitle = [FXLabel new];
         self.subtitle.backgroundColor = [UIColor clearColor];
         self.subtitle.textColor = [[SFStyleManager sharedManager] secondaryTextColor];
-        self.subtitle.font = [[SFStyleManager sharedManager] detailFontOfSize:((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) ? 22.0 : 18.0)];
+        self.subtitle.font = [[SFStyleManager sharedManager] detailFontOfSize:((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) ? 22.0 : 20.0)];
         self.subtitle.numberOfLines = 0;
         self.subtitle.textAlignment = NSTextAlignmentCenter;
         self.subtitle.gradientStartColor = [UIColor colorWithHexString:@"828282"];
@@ -59,6 +61,7 @@
         self.subtitle.innerShadowOffset = CGSizeMake(0.0, 1.0);
         self.subtitle.shadowColor = [UIColor blackColor];
         self.subtitle.shadowOffset = CGSizeMake(0.0, 2.0);
+        self.subtitle.layer.masksToBounds = NO;
         [self addSubview:self.subtitle];
         
 #if defined(LAYOUT_DEBUG)
@@ -76,7 +79,7 @@
     [super layoutSubviews];
     
     BOOL iphone568 = (UIScreen.mainScreen.bounds.size.height == 568.0);
-    CGSize padding = ((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) ? (UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation]) ? CGSizeMake(200.0, 200.0) : CGSizeMake(340.0, 120.0)) : (iphone568 ? CGSizeMake(20.0, 80.0) : CGSizeMake(20.0, 50.0)));
+    CGSize padding = ((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) ? (UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation]) ? CGSizeMake(200.0, 200.0) : CGSizeMake(340.0, 120.0)) : (iphone568 ? CGSizeMake(30.0, 60.0) : CGSizeMake(30.0, 30.0)));
     
     CGFloat maxWidth = CGRectGetWidth(self.frame) - (padding.width * 2.0);
     
@@ -86,13 +89,6 @@
     titleFrame.origin.x = nearbyintf((CGRectGetWidth(self.frame) / 2.0) - (CGRectGetWidth(titleFrame) / 2.0));
     titleFrame.origin.y = padding.height;
     self.title.frame = titleFrame;
-    
-    CGSize iconSize = [self.icon.text sizeWithFont:self.icon.font constrainedToSize:CGSizeMake(maxWidth, CGFLOAT_MAX) lineBreakMode:self.icon.lineBreakMode];
-    CGRect iconFrame = self.icon.frame;
-    iconFrame.size = iconSize;
-    iconFrame.origin.x = nearbyintf((CGRectGetWidth(self.frame) / 2.0) - (CGRectGetWidth(iconFrame) / 2.0));
-    iconFrame.origin.y = nearbyintf((CGRectGetHeight(self.frame) / 2.0) - CGRectGetHeight(iconFrame)) + nearbyintf(self.icon.font.lineHeight / 1.8);
-    self.icon.frame = iconFrame;
 
     CGSize subtitleSize = [self.subtitle.text sizeWithFont:self.subtitle.font constrainedToSize:CGSizeMake(maxWidth, CGFLOAT_MAX) lineBreakMode:self.subtitle.lineBreakMode];
     CGRect subtitleFrame = self.subtitle.frame;
@@ -100,6 +96,15 @@
     subtitleFrame.origin.x = nearbyintf((CGRectGetWidth(self.frame) / 2.0) - (CGRectGetWidth(subtitleFrame) / 2.0));
     subtitleFrame.origin.y = (CGRectGetHeight(self.frame) - CGRectGetHeight(subtitleFrame) - padding.height);
     self.subtitle.frame = subtitleFrame;
+    
+    CGSize iconSize = [self.icon.text sizeWithFont:self.icon.font constrainedToSize:CGSizeMake(maxWidth, CGFLOAT_MAX) lineBreakMode:self.icon.lineBreakMode];
+    CGRect iconFrame = self.icon.frame;
+    iconFrame.size = iconSize;
+    iconFrame.origin.x = nearbyintf((CGRectGetWidth(self.frame) / 2.0) - (CGRectGetWidth(iconFrame) / 2.0));
+    
+    CGFloat centerDistance = nearbyintf(CGRectGetMinY(subtitleFrame) - CGRectGetMaxY(titleFrame));
+    iconFrame.origin.y = nearbyintf((CGRectGetMaxY(titleFrame) + (centerDistance / 2.0)) - (CGRectGetHeight(iconFrame)) + nearbyintf(self.icon.font.lineHeight / 1.8));
+    self.icon.frame = iconFrame;
 }
 
 @end
